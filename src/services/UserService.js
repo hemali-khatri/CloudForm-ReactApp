09ec41c638fa6fd881ca9584e82906400feb856c
Hemali Khatri <hemali.khatri@tatvasoft.com>
2025-08-28 15:49:15 +0530

    changes

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,7 +1,8 @@
 // src/services/UserService.js
 import axios from 'axios';
 
-const apiUrl = 'http://localhost:3000/api/users';
+//const apiUrl = 'http://localhost:3000/api/users';
+const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000/api/users';
 
 export const getUsers = async () => {
   try {
